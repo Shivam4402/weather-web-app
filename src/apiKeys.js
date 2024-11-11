866c9019d1a0f0effad514d92252ba923a8dde29
Shivam4402 <shivamtikone8@gmail.com>
2024-11-11 09:52:38 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "8fdd683199a47ee029ad0432793881b9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
